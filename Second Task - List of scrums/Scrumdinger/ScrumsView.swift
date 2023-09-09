34ff3e235ed54c39d9828faf2421d9c013e1e090
Sinzolo <77647002+Sinzolo@users.noreply.github.com>
2023-09-09 17:30:14 +0100

    Added IDs to each daily scrum. Finished this part of the tutorial. So now app boots into a list of the current scrums.

--- Second Task - List of scrums/Scrumdinger/ScrumsView.swift
+++ Second Task - List of scrums/Scrumdinger/ScrumsView.swift
@@ -7,16 +7,18 @@
 
 import SwiftUI
 
+
 struct ScrumsView: View {
-    let scrums : [DailyScrum]
+    let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums, id: \.title) { scrum in
+        List(scrums) { scrum in
             CardView(scrum: scrum)
                 .listRowBackground(scrum.theme.mainColor)
         }
     }
 }
+
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
