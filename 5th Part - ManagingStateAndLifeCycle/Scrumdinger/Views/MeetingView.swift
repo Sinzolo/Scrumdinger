f4c3588957b7fc1a1c7c3c1cf3fae649b51ba307
Sinzolo <77647002+Sinzolo@users.noreply.github.com>
2023-09-13 13:32:12 +0100

    Finished Part 5 - Footer of meeting view now displays the speaker speaking and can now skip a speaker. Also, ding noise plays when time has elasped for a speaker.

--- 5th Part - ManagingStateAndLifeCycle/Scrumdinger/Views/MeetingView.swift
+++ 5th Part - ManagingStateAndLifeCycle/Scrumdinger/Views/MeetingView.swift
@@ -3,33 +3,33 @@
  */
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
-    @StateObject var scrumTimer = ScrumTimer()  /// Does the timing of the scrum
+    /// Does the timing of the scrum
+    @StateObject var scrumTimer = ScrumTimer()
     
+    /// Ding noise
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
     var body: some View {
     ZStack {
             RoundedRectangle(cornerRadius: 16)
                 .fill(scrum.theme.mainColor)
             VStack {
-                /// Displays the view for the time bar and the seconds
                 MeetingHeaderView(secondsElasped: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 Circle()
                     .strokeBorder(lineWidth: 24)
-                HStack {
-                    Text("Speaker 1 of 3")
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "forward.fill")
-                    }
-                    .accessibilityLabel("Next speaker")
-                }
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
             .padding()
             .foregroundColor(scrum.theme.accentColor)
             .onAppear { /// When the MeetingView is shown, reset and start the scrum timer
                 scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+                scrumTimer.speakerChangedAction = {
+                    player.seek(to: .zero)
+                    player.play()
+                }
                 scrumTimer.startScrum()
             }
             .onDisappear {  /// Stop scrum timer on view dissapearing
